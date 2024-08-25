0f73547e94799d72e26d0d519f959d6394b55102
Coby-Amar <cobyamar@gmail.com>
2024-08-24 17:51:26 -0700

    fix key

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'http://api.weatherapi.com/v1/forecast.json',
     params: {
-        'key': '371a8712e805433f8d523256242108',
+        'key': process.env.WEATHER_ACCESS_KEY,
         'days': 7
     },
 })
